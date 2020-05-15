7505adca853ff44a3d0c15e092cc8101a9c5fb43
alina.timonina172372 <alina.timonina172372@gmail.com>
2020-05-15 18:36:56 +0300

    Добавьте файлы проекта.

---
+++ PP(Z7_WinForms)/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace PP_Z7_WinForms_
+{
+    partial class Praktika_7
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Zadanie_1 = new System.Windows.Forms.GroupBox();
+            this.Zadanie_2 = new System.Windows.Forms.GroupBox();
+            this.str = new System.Windows.Forms.Label();
+            this.enter_str = new System.Windows.Forms.RichTextBox();
+            this.substr = new System.Windows.Forms.Label();
+            this.enter_substr = new System.Windows.Forms.RichTextBox();
+            this.str1 = new System.Windows.Forms.Label();
+            this.Search = new System.Windows.Forms.Button();
+            this.add_str = new System.Windows.Forms.Label();
+            this.str2 = new System.Windows.Forms.Label();
+            this.enter_str2 = new System.Windows.Forms.RichTextBox();
+            this.kolvo = new System.Windows.Forms.Label();
+            this.Num = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Button();
+            this.Zadanie_1.SuspendLayout();
+            this.Zadanie_2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Zadanie_1
+            // 
+            this.Zadanie_1.Controls.Add(this.add_str);
+            this.Zadanie_1.Controls.Add(this.Search);
+            this.Zadanie_1.Controls.Add(this.str1);
+            this.Zadanie_1.Controls.Add(this.enter_substr);
+            this.Zadanie_1.Controls.Add(this.substr);
+            this.Zadanie_1.Controls.Add(this.enter_str);
+            this.Zadanie_1.Controls.Add(this.str);
+            this.Zadanie_1.Location = new System.Drawing.Point(0, 0);
+            this.Zadanie_1.Name = "Zadanie_1";
+            this.Zadanie_1.Size = new System.Drawing.Size(346, 323);
+            this.Zadanie_1.TabIndex = 0;
+            this.Zadanie_1.TabStop = false;
+            this.Zadanie_1.Text = "Задание 1";
+            // 
+            // Zadanie_2
+            // 
+            this.Zadanie_2.Controls.Add(this.Result);
+            this.Zadanie_2.Controls.Add(this.Num);
+            this.Zadanie_2.Controls.Add(this.kolvo);
+            this.Zadanie_2.Controls.Add(this.enter_str2);
+            this.Zadanie_2.Controls.Add(this.str2);
+            this.Zadanie_2.Location = new System.Drawing.Point(352, 0);
+            this.Zadanie_2.Name = "Zadanie_2";
+            this.Zadanie_2.Size = new System.Drawing.Size(346, 323);
+            this.Zadanie_2.TabIndex = 0;
+            this.Zadanie_2.TabStop = false;
+            this.Zadanie_2.Text = "Задание 2 ";
+            // 
+            // str
+            // 
+            this.str.AutoSize = true;
+            this.str.Location = new System.Drawing.Point(6, 27);
+            this.str.Name = "str";
+            this.str.Size = new System.Drawing.Size(139, 17);
+            this.str.TabIndex = 0;
+            this.str.Text = "Введите строку str: ";
+            // 
+            // enter_str
+            // 
+            this.enter_str.Location = new System.Drawing.Point(9, 47);
+            this.enter_str.Name = "enter_str";
+            this.enter_str.Size = new System.Drawing.Size(331, 42);
+            this.enter_str.TabIndex = 1;
+            this.enter_str.Text = "";
+            // 
+            // substr
+            // 
+            this.substr.AutoSize = true;
+            this.substr.Location = new System.Drawing.Point(6, 110);
+            this.substr.Name = "substr";
+            this.substr.Size = new System.Drawing.Size(186, 17);
+            this.substr.TabIndex = 2;
+            this.substr.Text = "Введите подстроку substr: ";
+            // 
+            // enter_substr
+            // 
+            this.enter_substr.Location = new System.Drawing.Point(9, 130);
+            this.enter_substr.Name = "enter_substr";
+            this.enter_substr.Size = new System.Drawing.Size(331, 42);
+            this.enter_substr.TabIndex = 3;
+            this.enter_substr.Text = "";
+            // 
+            // str1
+            // 
+            this.str1.AutoSize = true;
+            this.str1.Location = new System.Drawing.Point(6, 195);
+            this.str1.Name = "str1";
+            this.str1.Size = new System.Drawing.Size(167, 17);
+            this.str1.TabIndex = 4;
+            this.str1.Text = "Измененная строка str: ";
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(86, 266);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(168, 47);
+            this.Search.TabIndex = 6;
+            this.Search.Text = "Поиск";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // add_str
+            // 
+            this.add_str.AutoSize = true;
+            this.add_str.Location = new System.Drawing.Point(6, 223);
+            this.add_str.Name = "add_str";
+            this.add_str.Size = new System.Drawing.Size(0, 17);
+            this.add_str.TabIndex = 7;
+            // 
+            // str2
+            // 
+            this.str2.AutoSize = true;
+            this.str2.Location = new System.Drawing.Point(6, 27);
+            this.str2.Name = "str2";
+            this.str2.Size = new System.Drawing.Size(139, 17);
+            this.str2.TabIndex = 8;
+            this.str2.Text = "Введите строку str: ";
+            // 
+            // enter_str2
+            // 
+            this.enter_str2.Location = new System.Drawing.Point(9, 47);
+            this.enter_str2.Name = "enter_str2";
+            this.enter_str2.Size = new System.Drawing.Size(331, 42);
+            this.enter_str2.TabIndex = 9;
+            this.enter_str2.Text = "";
+            // 
+            // kolvo
+            // 
+            this.kolvo.AutoSize = true;
+            this.kolvo.Location = new System.Drawing.Point(53, 110);
+            this.kolvo.Name = "kolvo";
+            this.kolvo.Size = new System.Drawing.Size(227, 34);
+            this.kolvo.TabIndex = 10;
+            this.kolvo.Text = "Кол-во слов, содержащие только\r\nпрописные буквы: ";
+            // 
+            // Num
+            // 
+            this.Num.AutoSize = true;
+            this.Num.Location = new System.Drawing.Point(190, 127);
+            this.Num.Name = "Num";
+            this.Num.Size = new System.Drawing.Size(0, 17);
+            this.Num.TabIndex = 11;
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(84, 180);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(168, 47);
+            this.Result.TabIndex = 12;
+            this.Result.Text = "Результат";
+            this.Result.UseVisualStyleBackColor = true;
+            this.Result.Click += new System.EventHandler(this.Result_Click);
+            // 
+            // Praktika_7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 325);
+            this.Controls.Add(this.Zadanie_2);
+            this.Controls.Add(this.Zadanie_1);
+            this.Name = "Praktika_7";
+            this.Text = "Практика 7";
+            this.Zadanie_1.ResumeLayout(false);
+            this.Zadanie_1.PerformLayout();
+            this.Zadanie_2.ResumeLayout(false);
+            this.Zadanie_2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Zadanie_1;
+        private System.Windows.Forms.GroupBox Zadanie_2;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Label str1;
+        private System.Windows.Forms.RichTextBox enter_substr;
+        private System.Windows.Forms.Label substr;
+        private System.Windows.Forms.RichTextBox enter_str;
+        private System.Windows.Forms.Label str;
+        private System.Windows.Forms.Label add_str;
+        private System.Windows.Forms.Label Num;
+        private System.Windows.Forms.Label kolvo;
+        private System.Windows.Forms.RichTextBox enter_str2;
+        private System.Windows.Forms.Label str2;
+        private System.Windows.Forms.Button Result;
+    }
+}
+
---
+++ PP(Z7_WinForms)/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PP_Z7_WinForms_
+{
+    public partial class Praktika_7 : Form
+    {
+        public Praktika_7()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            StringBuilder str = new StringBuilder(enter_str.Text);
+            StringBuilder substr = new StringBuilder(enter_substr.Text);
+            str.Replace(substr.ToString(), "");
+            add_str.Text = str.ToString();
+        }
+        static int numberOfUpWords(StringBuilder input)
+        {
+            int ret = 0, ind = 0;
+            while (ind < input.Length)
+            {
+                int nUCase = 0, nLCase = 0;
+                while (ind < input.Length && char.IsLetter(input[ind]))
+                {
+                    if (char.IsUpper(input[ind])) nUCase++;
+                    if (char.IsLower(input[ind])) nLCase++;
+                    ind++;
+                }
+                if (nUCase > 0 && nLCase == 0) ret++;
+                while (ind < input.Length && !char.IsLetter(input[ind]))
+                    ind++;
+            }
+            return ret;
+        }
+
+        private void Result_Click(object sender, EventArgs e)
+        {
+            StringBuilder str = new StringBuilder(enter_str2.Text);
+            Num.Text = numberOfUpWords(str).ToString();
+        }
+    }
+}
